eb7f34f3a6a44c16902c86efb01df60a0e171b78
RITESH-MESHRAM <riteshpm1998@gmail.com>
2025-06-15 15:36:40 +0530

    commited

---
+++ src/packagex/Class11.java
@@ -0,0 +1,14 @@
+package packagex;
+
+public class Class11 {
+
+	public static void main(String[] args) {
+
+		  String s = "ritesh";
+		  String reversed = new StringBuilder(String.valueOf(s)).reverse().toString();
+
+		  System.out.println(reversed);
+		
+	
+	}
+}
---
+++ src/packagex/PracticeClass.java
@@ -0,0 +1,12 @@
+package packagex;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+	}
+
+}
